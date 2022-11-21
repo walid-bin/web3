5b78bceca5815a5f6f0c67b00f2b58e123690290
walid-bin <jashim@coredevs.ltd>
2022-11-21 12:49:08 +0600

    final

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+// imports web3 requirements
 const Web3 = require('web3');
 const abi = require('./abi.json');
 const web3 = new Web3('ws://localhost:7545');
-web3.eth.defaultAccount = '0x43dd88e9858846401A2aBE3F57F2422d8c65D008';
-const contractAddress = '0x88172DBe28E77e47060E42c0B8ed5EC245Bf1f3f';
-const walletFor = "0x6E84150012Fd6D571C33C266424fcDEcF80E3274";
-const contract = new web3.eth.Contract(abi, contractAddress);
-(async function () {
-    // console.log(await web3.getAccounts()
-    console.log(web3.eth.defaultAccount);
+require('dotenv').config();
 
+// initialize require data
+web3.eth.defaultAccount = process.env.DEFAULT_WALLET_ADDRESS || '0x43dd88e9858846401A2aBE3F57F2422d8c65D008';
+const contractAddress = process.env.CONTRACT_ADDRESS || '0x88172DBe28E77e47060E42c0B8ed5EC245Bf1f3f';
+const walletFor = process.env.TARGET_WALLET || "0x6E84150012Fd6D571C33C266424fcDEcF80E3274";
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+
+// async function to execute 
+(async function () {
+    // checks default account
+    console.log(process.env.DEFAULT_WALLET_ADDRESS);
+
+    // catching pending request from rhe block chain, and send mint method.
     var subscription = web3.eth.subscribe('pendingTransactions', function (error, result) {
         if (!error)
             console.log("result:");
@@ -27,8 +35,9 @@ const contract = new web3.eth.Contract(abi, contractAddress);
         });
 
 
-    contract.events.Transfer({
 
+    // catching transfer event from rhe block chain
+    contract.events.Transfer({
     }, function (error, event) { console.log("event"); })
         .on("connected", function (subscriptionId) {
             console.log(subscriptionId);
