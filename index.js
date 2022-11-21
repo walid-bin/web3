f7a43cf10f9f7cb55a950cddd0018090ffd5a65a
walid-bin <jashim@coredevs.ltd>
2022-11-21 18:59:52 +0600

    final

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // imports web3 requirements
 const Web3 = require('web3');
 const abi = require('./abi.json');
-const web3 = new Web3('ws://localhost:7545');
+const web3 = new Web3('ws://10.11.70.174:7545');
 require('dotenv').config();
 
 // initialize require data
-web3.eth.defaultAccount = process.env.DEFAULT_WALLET_ADDRESS || '0x43dd88e9858846401A2aBE3F57F2422d8c65D008';
-const contractAddress = process.env.CONTRACT_ADDRESS || '0x88172DBe28E77e47060E42c0B8ed5EC245Bf1f3f';
+web3.eth.defaultAccount = process.env.DEFAULT_WALLET_ADDRESS || '0x293c14d5a84A35eFef6ad46D05DC901Bf0729a3F';
+const contractAddress = process.env.CONTRACT_ADDRESS || '0xd447e9a17F2CDd75cA9F0b69CF1B7D89237A05F5';
 const walletFor = process.env.TARGET_WALLET || "0x6E84150012Fd6D571C33C266424fcDEcF80E3274";
 const contract = new web3.eth.Contract(abi, contractAddress);
 
@@ -26,9 +26,10 @@ const contract = new web3.eth.Contract(abi, contractAddress);
             web3.eth.getTransaction(transaction, function (err, data) {
                 if (err) return console.log(err);
                 // console.log(data);
+                if (data.from === web3.eth.defaultAccount) return console.log('the Transection is your.')
                 data.from === walletFor && contract.methods.mint().send({
                     from: web3.eth.defaultAccount,
-                    gas: data.gas * 2,
+                    gas: data.gas * 4,
                     gasPrice: (parseInt(data.gas) + 1000).toString()
                 })
             })
